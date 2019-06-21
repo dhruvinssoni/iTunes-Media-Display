b16d5ceca4a0aef5c3e16c9b2aebd255db33f9ff
Dhruvin Soni <dhruvin@Dhruvins-MacBook-Air.local>
2019-06-21 14:34:12 -0500

    Added tableview and displayed artwork, also added constraints for autolayout

--- iTunes Media/Controller/ViewController.swift
+++ iTunes Media/Controller/ViewController.swift
@@ -8,20 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //Constants
     let store = DataStore.sharedInstance
     
+    //Variables
+    var tableview: UITableView = {
+        let myTableView = UITableView()
+        myTableView.backgroundColor = UIColor.white
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return myTableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        store.getMusic {
-            print("data fetched")
+        setupTableView()
+        store.getartWorkImages {
+            self.tableview.reloadData()
         }
         // Do any additional setup after loading the view.
     }
-
+    
+    func setupTableView() {
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        tableview.allowsSelection = false
+        
+        self.view.addSubview(tableview)
+        
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableview.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableview.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+            ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return store.applemusic.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cellID")
+        cell.backgroundColor = UIColor.white
+        
+        let musicData = store.applemusic[indexPath.row]
+        
+        cell.textLabel?.text = "artist"
+        cell.imageView!.image = store.images[indexPath.row]
+        cell.detailTextLabel?.text = "album" 
+        return cell
+    }
 
 }
 
